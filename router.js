cd395e5aadeef40cbabfdad74e3433cec288a96a
samuel dazad <samueldazadaza@gmail.com>
2021-04-08 13:05:16 -0500

    Haciendo match entre la URL y una ruta9/12

--- router.js
+++ router.js
@@ -4,6 +4,21 @@ class Router {
         this.routes = routes;
         this._loadInitialRoute();
     }
+
+_matchUrlToRoute(urlSegs){
+    const matchRoute = this.routes.find(route => {
+        
+        const routePathSegs = route.path.split('/').slice(1)
+
+        if (routePathSegs.length !== urlSegs.length) {
+            return false;
+        }
+
+        return routePathSegs
+            .every((routePathSeg, i) => routePathSeg === urlSegs.[i]);
+    });
+
+    return matchRoute;
 }
 
 _loadInitialRoute() {
@@ -11,5 +26,6 @@ _loadInitialRoute() {
     const pathNameSplit = window.location.pathname.split('/');
     const pathSegs = pathNameSplit.length > 1 ? pathNameSplit(1): '';
 
-    this.loadRoute(...pathSegs)
+    this.loadRoute(...pathSegs);
+    }
 }
