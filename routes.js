204c6518e95003debe25219a15db0329d01bb805
samuel dazad <samueldazadaza@gmail.com>
2021-04-08 11:42:42 -0500

    Creando nuestro archivo de rutas7/12

---
+++ routes.js
@@ -0,0 +1,14 @@
+const routes = [
+    {
+        path: '/',
+        template: <h1>Hola</h1>
+    },
+    {
+        path: '/contacto',
+        template: <h1>Contacto</h1>
+    },
+    {
+        path: '/aboutme',
+        template: <h1>About me</h1>
+    },
+];
